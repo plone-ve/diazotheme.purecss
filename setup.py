dddcf9757e335ecd2f0ea4199a1a5481eecce3df
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-07-02 18:30:08 -0400

    Added more strings classifiers items for the metadata package

--- setup.py
+++ setup.py
@@ -1,21 +1,39 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = '1.1'
 
 setup(name='diazotheme.purecss',
       version=version,
-      description="",
+      description="A Diazo Themes implementation for Pure CSS",
       long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: Theme",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
-      author='TH-code',
+      keywords='web zope plone theme diazo pure css framework',
+      author='Thijs Jonkman',
       author_email='t.jonkman@gmail.com',
-      url='https://github.com/TH-code',
+      maintainer='Leonardo Caballero',
+      maintainer_email='leonardocaballero@gmail.com',
+      url='https://github.com/TH-code/diazotheme.purecss',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['diazotheme'],
