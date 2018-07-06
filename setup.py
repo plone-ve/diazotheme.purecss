a99f775de3d9f5fa76675679deee2491ee12989d
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-07-05 21:34:37 -0400

    Added improvements about README file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.1'
 setup(name='diazotheme.purecss',
       version=version,
       description="A Diazo Themes implementation for Pure CSS",
-      long_description=open("README.md").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
         "Development Status :: 4 - Beta",
